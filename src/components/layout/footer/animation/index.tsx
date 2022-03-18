cdc6ec100686e1e645412efd975b4cf9ec9daaa9
UwUssimo Robinson <uwussimo@icloud.com>
2022-03-19 00:56:37 +0500

    Update index.tsx

--- src/components/layout/footer/animation/index.tsx
+++ src/components/layout/footer/animation/index.tsx
@@ -43,7 +43,7 @@ const Letter = styled('span', {
   }
 })
 
-const LETTERS = ['{', '}', '<', '>', '/', '\\', '|']
+const LETTERS = ['{', '}', '<', '>', '/']
 
 const FooterAnimation = () => {
   const animContainerRef = useRef<HTMLDivElement>(null)
